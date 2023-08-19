c59c93554e7ddb9bf045bca78c22decea9d26975
LukaGajic2 <>
2023-08-19 11:35:04 +0000

    Create first view and add some css style

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+    path('', views.about_page, name='about'),
+    path('blog/', views.BlogPage.as_view(), name='blog'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import BlogPost
 
-# Create your views here.
+
+def about_page(request):
+    """
+    View for about page.
+    """
+    return render(request, 'about.html')
+
+
+# Class used from "I think therefore I blog" walkthrough.
+class BlogPage(generic.ListView):
+    """
+    View for blog page.
+    """
+    model = BlogPost
+    queryset = BlogPost.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blog.html'
+    paginate_by = 6
