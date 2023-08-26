284df48273c4061afc7f37145ea383c4e7d442b3
LukaGajic2 <>
2023-08-26 11:01:18 +0000

    Create single post view with number of likes and number of comments

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.about_page, name='about'),
     path('blog/', views.BlogPage.as_view(), name='blog'),
+    path('<slug:slug>/', views.BlogPostPage.as_view(), name='post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -32,6 +32,7 @@ class BlogPostPage(View):
         queryset = BlogPost.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
         
