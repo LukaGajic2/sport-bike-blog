9cb557c044c08bcedc2ce31d535977380326a92f
LukaGajic2 <>
2023-12-18 15:48:03 +0000

    Customize admin page for EVENTS app

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import EventPost, EventComment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(EventPost)
+class EventPostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'location', 'status', 'created_on')
+    search_fields = ['title', 'description']
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('description')
+
+
+@admin.register(EventComment)
+class EventCommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
